eb375d16150c6fd0407d47a4080b781d780e84fa
Mohamed <46685829+Mohamed1414@users.noreply.github.com>
2021-10-07 18:09:05 +0200

    Add files via upload

--- swap_a.c
+++ swap_a.c
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   swap_a.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbahstou <mbahstou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/30 15:48:48 by mbahstou          #+#    #+#             */
-/*   Updated: 2021/10/07 17:53:36 by mbahstou         ###   ########.fr       */
+/*   Created: 2021/10/07 17:55:59 by mbahstou          #+#    #+#             */
+/*   Updated: 2021/10/07 18:07:54 by mbahstou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char *argv[])
+int	swap_a(int *stack_a)
 {
-	int *stack_a;
-	int *stack_b;
 	int i;
-	if(argc < 2)
-		return 0;
-	stack_a = malloc(sizeof(int) * argc);
-	stack_b = malloc(sizeof (int) * argc);
-	i = 1;
-	while (i < argc)
-	{
-		stack_a[i - 1] = ft_atoi(argv[i]);
-		i++;
-	}
-	stack_a[i - 1] = '\0';
-	stack_b[i - 1] = '\0';
-	stack_b[i] = '\0';
-	return (0);
+	i = stack_a[0];
 }
